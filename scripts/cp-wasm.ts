4b65ac267cd88f0524a1ae435b32b4296fbbb123
Gregor Weber <gregorwbr@gmail.com>
2022-12-30 21:50:57 +0100

    publish stuff

--- scripts/cp-wasm.ts
+++ scripts/cp-wasm.ts
@@ -2,12 +2,11 @@ import * as path from "path";
 
 import * as fs from "fs-extra";
 
+fs.mkdirpSync("out");
+
 fs.copyFileSync(
   path.join("node_modules", "web-tree-sitter", "tree-sitter.wasm"),
   path.join("out", "tree-sitter.wasm")
 );
 
-fs.copySync(
-  path.join("node_modules", "tree-sitter-wasms", "out"),
-  path.join("out")
-);
+fs.copySync(path.join("node_modules", "tree-sitter-wasms", "out"), "out");
